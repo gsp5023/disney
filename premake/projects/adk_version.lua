8fb112c377555c4a54a1d43c72810f777bf494c2
Andrew Bare <andrew.bare@disneystreaming.com>
2022-03-04 18:09:42 -0800

    v1.2.5+nve.2.1.18+crate-2.0.5-a8372225dc4a11007704afc9046dfbd2b690f4eb

--- premake/projects/adk_version.lua
+++ premake/projects/adk_version.lua
@@ -2,4 +2,4 @@
 -- adk_version.lua
 -- Copyright (c) 2019-2020 Disney Streaming Technology LLC. All rights reserved.
 --------------------------------------------------------------------------------
-defines { "ADK_VERSION_STRING=\"1.2.5+nve.2.1.17+crate-2.0.4+dba7704.2\"" }
+defines { "ADK_VERSION_STRING=\"1.2.5+nve.2.1.18+crate-2.0.5+a837222.4\"" }
